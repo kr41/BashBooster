0902886679adfdf4bcb669020b7633e8e9b3d984
Dmitry Vakhrushev <self@kr41.net>
2014-07-09 08:51:19 +0700

    Added test for workspace location

--- unit tests/test-workspace.sh
+++ unit tests/test-workspace.sh
@@ -7,3 +7,4 @@ BB_LOG_PREFIX='test-workspace'
 source ../bashbooster.sh
 
 [[ -d "$BB_WORKSPACE" ]] || die 1 "Workspace doesn't exist"
+[[ -d ".bb-workspace" ]] || die 2 "Invalid workspace location"
