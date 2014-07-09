f4eb447b790ec5607c464b87673fe44f831026e5
Dmitry Vakhrushev <self@kr41.net>
2014-07-09 15:41:44 +0700

    Added sync dir function

--- source/13_sync.sh
+++ source/13_sync.sh
@@ -13,3 +13,39 @@ bb-sync-file() {
         bb-event-delay "$EVENT"
     fi
 }
+
+bb-sync-dir() {
+    local DST_DIR="$1"
+    local SRC_DIR="$2"
+    local EVENT="$3"
+    if [[ ! -d "$DST_DIR" ]]
+    then
+        mkdir -p "$DST_DIR"
+        bb-event-delay "$EVENT"
+    fi
+
+    local ORIGINAL_DIR=`pwd`
+
+    cd "$SRC_DIR"
+    for NAME in `ls`
+    do
+        if [[ -f "$SRC_DIR/$NAME" ]]
+        then
+            bb-sync-file "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$EVENT"
+        elif [[ -d "$SRC_DIR/$NAME" ]]
+        then
+            bb-sync-dir "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$EVENT"
+        fi
+    done
+    cd "$DST_DIR"
+    for FILE in `find`
+    do
+        if [[ ! -e "$SRC_DIR/$FILE" ]]
+        then
+            rm -rf "$DST_DIR/$FILE"
+            bb-event-delay "$EVENT"
+        fi
+    done
+
+    cd "$ORIGINAL_DIR"
+}
--- unit tests/test-sync-file.sh
+++ unit tests/test-sync-file.sh
@@ -3,19 +3,24 @@
 unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+BB_LOG_PREFIX='sync-file'
+BB_LOG_LEVEL=$BB_LOG_DEBUG
 source ../bashbooster.sh
 
 SRC_FILE=`bb-tmp-file`
 DST_FILE="$BB_WORKSPACE/testfile"
-bb-event-listen file-changed 'echo "File changed"'
+bb-event-listen bb-cleanup 'rm "$DST_FILE"'
+bb-event-listen file-changed-1 'echo "File changed 1"'
+bb-event-listen file-changed-2 'echo "File changed 2"'
+bb-event-listen file-changed-3 'echo "File changed 3"'
 
 echo "Foo" >> "$SRC_FILE"
-bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed'
-[[ ! -f "$DST_FILE" ]] && bb-die 1 "File doesn't exist"
-[[ -n `diff -q $DST_FILE $SRC_FILE` ]] && bb-die 2 "Files are different"
+bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed-1'
+[[ -f "$DST_FILE" ]] || bb-die 1 "File doesn't exist"
+[[ ! -n `diff -q "$DST_FILE" "$SRC_FILE"` ]] || bb-die 2 "Files are different"
 
 echo "Bar" >> "$SRC_FILE"
-bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed'
-[[ -n `diff -q $DST_FILE $SRC_FILE` ]] && bb-die 2 "Files are different"
+bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed-2'
+[[ ! -n `diff -q "$DST_FILE" "$SRC_FILE"` ]] || bb-die 2 "Files are different"
 
-bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed'
+bb-sync-file "$DST_FILE" "$SRC_FILE" 'file-changed-3'
