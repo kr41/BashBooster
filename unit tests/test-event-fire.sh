862825dbffdd5d31069424f053b39e1d7bf293dc
Dmitry Vakhrushev <self@kr41.net>
2014-07-09 09:31:01 +0700

    Updated event test to test handler with space chars in command

--- unit tests/test-event-fire.sh
+++ unit tests/test-event-fire.sh
@@ -6,10 +6,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 BB_LOG_PREFIX='test-event'
 source ../bashbooster.sh
 
-event-handler() {
-    echo "Event processed"
-}
-
 bb-event-fire event
-bb-event-listen event event-handler
+bb-event-listen event 'echo "Event processed"'
 bb-event-fire event
