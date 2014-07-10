507afad42ddbe32c04a91aca228f8ed23d8d1cf5
Dmitry Vakhrushev <self@kr41.net>
2014-07-11 03:46:53 +0700

    Added logging to download module

--- source/30_download.sh
+++ source/30_download.sh
@@ -3,6 +3,7 @@ BB_DOWNLOAD_DIR="$BB_WORKSPACE/download"
 bb-download() {
     if [[ ! -d "$BB_DOWNLOAD_DIR" ]]
     then
+        bb-log-debug "Creating download directory at '$BB_DOWNLOAD_DIR'"
         mkdir "$BB_DOWNLOAD_DIR"
     fi
 
@@ -14,6 +15,7 @@ bb-download() {
     fi
     TARGET="$BB_DOWNLOAD_DIR/$TARGET"
 
+    bb-log-info "Downloading $URL"
     wget -O "$TARGET" -nc "$URL"
     echo "$TARGET"
 }
