03986771d7b3944cb356e3ec4f690534e657926f
Dmitry Vakhrushev <self@kr41.net>
2014-07-11 21:05:19 +0700

    Added flag module

--- source/99_init.sh
+++ source/99_init.sh
@@ -2,10 +2,12 @@ bb-workspace-init
 bb-tmp-init
 bb-event-init
 bb-download-init
+bb-flag-init
 
 bb-cleanup() {
     bb-event-fire bb-cleanup
 
+    bb-flag-cleanup
     bb-event-cleanup
     bb-tmp-cleanup
     bb-workspace-cleanup
