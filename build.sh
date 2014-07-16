ea48723c8f0ee2c506372443883914597d200bde
Dmitry Vakhrushev <self@kr41.net>
2014-07-16 22:45:20 +0700

    Added preamble and version

--- build.sh
+++ build.sh
@@ -4,11 +4,21 @@ unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 OUTPUT="bashbooster.sh"
-
 > $OUTPUT
+
+TITLE="Bash Booster $( cat VERSION.txt ) <http://www.bashbooster.net>"
+cat >> "$OUTPUT" <<EOF
+# $TITLE
+# $( eval "printf '=%.0s' {1..${#TITLE}}" )
+#
+$( cat LICENSE.txt | awk '{ print "# "$0 }' )
+#
+
+EOF
+
 for FILE in $( find ./source -name "*.sh" | sort )
 do
-    echo -e "##\n# $FILE\n#\n" >> $OUTPUT
-    cat $FILE >> $OUTPUT
-    echo >> $OUTPUT
+    echo -e "##\n# $FILE\n#\n" >> "$OUTPUT"
+    cat "$FILE" >> "$OUTPUT"
+    echo >> "$OUTPUT"
 done
