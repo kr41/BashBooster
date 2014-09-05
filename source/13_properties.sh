280d42cc205253cfa7e04f75bae1195e088a9efe
Denis Nelubin <dnelubin@gmail.com>
2014-09-05 22:38:23 +0700

    Added a first version of properties module.

---
+++ source/13_properties.sh
@@ -0,0 +1,15 @@
+bb-properties-read() {
+    local FILENAME="$1"
+    local PREFIX="$2"
+
+    # normalizing, reading and evaluating key=value lines from the properties file
+    # regexp searches for lines with key=value, key:value, key: value etc.. pattern, see http://docs.oracle.com/javase/7/docs/api/java/util/Properties.html#load(java.io.Reader)
+    # gawk normalizes such lines to key="value" form
+    # eval executes the key="value" definitions to define Bash variables
+    while read line
+    do
+#        echo "$line"
+        eval "$line"
+    done < <(gawk 'match($0, /^([^[:blank:]#!][^[:blank:]:=]*)[[:blank:]:=]+(.+)[[:blank:]]*/, m) \
+                   { print "'$PREFIX'" m[1] "=\"" m[2] "\"" }' $FILENAME)
+}
---
+++ unit tests/properties/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+unset CDPATH
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+source ../../bashbooster.sh
+
+bb-assert-empty () {
+    local ACTUAL="$1"
+    local MESSAGE="$2"
+
+    [[ -z "$ACTUAL" ]] || bb-exit 1 "$MESSAGE expected emplty, but was:<$ACTUAL>"
+}
+
+bb-assert-equals () {
+    local ACTUAL="$1"
+    local EXPECTED="$2"
+    local MESSAGE="$3"
+
+    [[ "$ACTUAL" == "$EXPECTED" ]] || bb-exit 1 "$MESSAGE expected:<$EXPECTED> but was:<$ACTUAL>"
+}
+
+bb-assert-empty "$p_key1"
+bb-assert-empty "$p_key2"
+bb-assert-empty "$p_key3"
+bb-assert-empty "$p_key4"
+bb-assert-empty "$p_key5"
+bb-assert-empty "$p_long"
+
+bb-properties-read test.properties p_
+
+bb-assert-equals "$p_key1" "value1"
+bb-assert-equals "$p_key2" "value2"
+bb-assert-equals "$p_key3" "value3"
+bb-assert-equals "$p_key4" "value4"
+bb-assert-equals "$p_key5" "value5"
+bb-assert-equals "$p_long" "string with spaces"
