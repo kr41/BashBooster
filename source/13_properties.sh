e45e5cf2002e8141d98250fc8a89506f37506251
Dmitry Vakhrushev <self@kr41.net>
2014-10-02 12:22:32 +0700

    Fixes #1 Replaced gawk parser by python one

--- source/13_properties.sh
+++ source/13_properties.sh
@@ -1,3 +1,24 @@
+# normalizing, reading and evaluating key=value lines from the properties file
+# regexp searches for lines with key=value, key:value, key: value etc.. pattern,
+# see http://docs.oracle.com/javase/7/docs/api/java/util/Properties.html#load(java.io.Reader)
+BB_PROPERTIES_READER=$( cat <<EOF
+import re
+import sys
+
+filename = sys.argv[1]
+prefix = sys.argv[2]
+with open(filename, 'r') as properties:
+    for line in properties:
+        line = line.strip()
+        match = re.match(r'^(?P<key>[^#!]*?)[\s:=]+(?P<value>.+)', line)
+        if match:
+            match = match.groupdict()
+            match['key'] = re.sub(r'[\W]', '_', match['key'])
+            print('{prefix}{key}="{value}"'.format(prefix=prefix, **match))
+EOF
+)
+
+
 bb-properties-read() {
     local FILENAME="$1"
     local PREFIX="$2"
@@ -8,19 +29,5 @@ bb-properties-read() {
         return
     fi
 
-    # normalizing, reading and evaluating key=value lines from the properties file
-    # regexp searches for lines with key=value, key:value, key: value etc.. pattern,
-    # see http://docs.oracle.com/javase/7/docs/api/java/util/Properties.html#load(java.io.Reader)
-    # gawk normalizes such lines to key="value" form
-    # eval executes the key="value" definitions to define Bash variables
-    while read line
-    do
-        eval "$line"
-    done < <(gawk 'match($0, \
-                        /^([^[:blank:]#!][^[:blank:]:=]*)[[:blank:]:=]+(.+)[[:blank:]]*/ \
-                        , m) \
-                        { \
-                            gsub(/[^a-zA-Z0-9_]/, "_", m[1]);
-                            print "'$PREFIX'" m[1] "=\"" m[2] "\"" \
-                        }' $FILENAME)
+    eval "$( python -c "$BB_PROPERTIES_READER" "$FILENAME" "$PREFIX" )"
 }
