0a878c888b2917e83b35062a8476f30e56f8be29
Dmitry Vakhrushev <self@kr41.net>
2014-10-10 12:08:24 +0700

    Added function for deprecated warning messages

---
+++ unit tests/01_log/deprecated_test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+unset CDPATH
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+source ../../bashbooster.sh
+
+
+EXPECTED_MESSAGE=""
+# Mock
+bb-log-warning() {
+    MESSAGE="$1"
+    bb-assert '[[ "$MESSAGE" == "$EXPECTED_MESSAGE" ]]'
+}
+
+
+old-func-1() {
+    bb-log-deprecated 'new-func-1'
+}
+
+old-func-2() {
+    bb-log-deprecated 'new-func-2' 'old-func-2'
+}
+
+EXPECTED_MESSAGE="'old-func-1' is deprecated, use 'new-func-1' instead"
+old-func-1
+
+EXPECTED_MESSAGE="'old-func-2' is deprecated, use 'new-func-2' instead"
+old-func-2
