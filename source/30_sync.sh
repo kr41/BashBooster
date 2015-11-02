40db92877d952904ef758d8f7448f16bd1992900
Jocelyn Le Sage <jocelyn@le-sage.com>
2015-11-02 10:40:06 -0500

    Delayed event arguments containing spaces where not handled properly by the sync module. Added unit test to cover this case.

--- source/30_sync.sh
+++ source/30_sync.sh
@@ -2,16 +2,15 @@ bb-sync-file() {
     local DST_FILE="$1"
     local SRC_FILE="$2"
     shift 2
-    local EVENT="$@"
     if [[ ! -f "$DST_FILE" ]]
     then
         touch "$DST_FILE"
-        bb-event-delay $EVENT
+        bb-event-delay "$@"
     fi
     if [[ -n "$( diff -q "$SRC_FILE" "$DST_FILE" )" ]]
     then
         cp -f "$SRC_FILE" "$DST_FILE"
-        bb-event-delay $EVENT
+        bb-event-delay "$@"
     fi
 }
 
@@ -19,11 +18,10 @@ bb-sync-dir() {
     local DST_DIR="$( readlink -nm "$1" )"
     local SRC_DIR="$( readlink -ne "$2" )"
     shift 2
-    local EVENT="$@"
     if [[ ! -d "$DST_DIR" ]]
     then
         mkdir -p "$DST_DIR"
-        bb-event-delay $EVENT
+        bb-event-delay "$@"
     fi
 
     local ORIGINAL_DIR="$( pwd )"
@@ -34,10 +32,10 @@ bb-sync-dir() {
     do
         if [[ -f "$SRC_DIR/$NAME" ]]
         then
-            bb-sync-file "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$EVENT"
+            bb-sync-file "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$@"
         elif [[ -d "$SRC_DIR/$NAME" ]]
         then
-            bb-sync-dir "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$EVENT"
+            bb-sync-dir "$DST_DIR/$NAME" "$SRC_DIR/$NAME" "$@"
         fi
     done < <( ls )
     cd "$DST_DIR"
@@ -46,7 +44,7 @@ bb-sync-dir() {
         if [[ ! -e "$SRC_DIR/$NAME" ]]
         then
             rm -rf "$DST_DIR/$NAME"
-            bb-event-delay $EVENT
+            bb-event-delay "$@"
         fi
     done < <( find . )
 
