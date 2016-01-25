d4f49ac94d157ae7a7aac78e1d10683490ea5ca7
Dmitry Vakhrushev <self@kr41.net>
2016-01-25 21:00:12 +0600

    Doesn't have to skip fail test

--- unit tests/30_download/fail-test.sh
+++ unit tests/30_download/fail-test.sh
@@ -5,9 +5,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 source "$BASHBOOSTER"
 
-timeout 5 wget -q --spider http://bashbooster.net/index.html
-[ $? -eq 0 ] || exit 255 # Skip test (No internet access?)
-
 # expect: STDERR='\[ERROR\] An error occurs while downloading http://localhost:666/file.txt'
 
 FILE="$( bb-download 'http://localhost:666/file.txt' )"
