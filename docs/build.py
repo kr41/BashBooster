bfcd67bbf06a432fd88da383e06a1a52e53d545b
Dmitry Vakhrushev <self@kr41.net>
2019-02-21 17:43:18 +0100

    Fixed Python 3/2 compatibility in doc-bulder

--- docs/build.py
+++ docs/build.py
@@ -6,32 +6,25 @@ from mako.template import Template
 here = os.path.dirname(os.path.abspath(__file__))
 
 markdown = Markdown(
-    extensions=['headerid', 'meta', 'extra', 'codehilite', 'toc'],
+    extensions=["headerid", "meta", "extra", "codehilite", "toc"],
     extension_configs={
-        'codehilite': [
-            ('linenums', False),
-            ('guess_lang', False),
-        ],
-        'toc': [
-            ('title', 'Table of Contents'),
-            ('permalink', True),
-        ]
-    }
+        "codehilite": [("linenums", False), ("guess_lang", False)],
+        "toc": [("title", "Table of Contents"), ("permalink", True)],
+    },
 )
-template = Template(filename=os.path.join(here, 'layout.mako'),
-                    output_encoding='utf-8')
-with open(os.path.join(here, 'index.md')) as f:
-    source = f.read().decode('utf-8')
+template = Template(filename=os.path.join(here, "layout.mako"), output_encoding="utf-8")
+with open(os.path.join(here, "index.md"), "rb") as f:
+    source = f.read().decode("utf-8")
 
-with open(os.path.join(here, '..', 'CHANGES.md')) as f:
-    source += '\n' + f.read().decode('utf-8')
+with open(os.path.join(here, "..", "CHANGES.md"), "rb") as f:
+    source += "\n" + f.read().decode("utf-8")
 
-with open(os.path.join(here, '..', 'VERSION.txt')) as f:
-    version = f.read().decode('utf-8')
+with open(os.path.join(here, "..", "VERSION.txt"), "rb") as f:
+    version = f.read().decode("utf-8")
 
 body = markdown.convert(source)
 meta = markdown.Meta
-meta['version'] = version
+meta["version"] = version
 
-with open(os.path.join(here, 'www', 'index.html'), 'w') as f:
+with open(os.path.join(here, "www", "index.html"), "wb") as f:
     f.write(template.render(body=body, meta=meta))
